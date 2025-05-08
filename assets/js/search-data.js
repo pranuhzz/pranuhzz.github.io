e97879f0339aeff4bb608c46127caf5425673bd7
pranuhzz <pranuhzz@gmail.com>
2025-05-08 06:45:54 +0000

    Deploying to gh-pages from @ pranuhzz/pranuhzz.github.io@0c2fbe850e32a8bf3f1c98feff1b803fe2e53b51 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-phoenix",
+          title: 'Phoenix',
+          description: "A drone that can locate the survivors to be rescued in fire accidents",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
